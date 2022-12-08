2aa1a59f14684a1d85791bfe0b4e93b34852059c
Atahar-Hossain <ataharhossain247@gmail.com>
2022-12-08 22:22:16 +0600

    new js added

--- js/array-methods.js
+++ js/array-methods.js
@@ -1,3 +1,10 @@
 const products = [
-    {name:`laptop`, brand: `lenovo`, price:32000, color:`silver`}
-]
+	{ name: `laptop`, brand: `lenovo`, price: 32000, color: `silver` },
+	{ name: `laptop`, brand: `lenovo`, price: 32000, color: `silver` },
+	{ name: `laptop`, brand: `lenovo`, price: 32000, color: `silver` },
+	{ name: `laptop`, brand: `lenovo`, price: 32000, color: `silver` },
+	{ name: `laptop`, brand: `lenovo`, price: 32000, color: `silver` },
+	{ name: `laptop`, brand: `lenovo`, price: 32000, color: `silver` },
+];
+const brands = products.map((product) => product.brand);
+console.log(brands);
