f86a60ad9bf22e3b16f56388f841c5ab1c176e68
Atahar-Hossain <ataharhossain247@gmail.com>
2022-12-09 21:17:02 +0600

    new js added

---
+++ shortcut.js
@@ -0,0 +1,11 @@
+/*
+01. truthy and falsy
+
+02. ternary operrator
+
+03. number to string without using stringify
+
+04. string to number without using parse
+
+05. see the module videos again
+ */
